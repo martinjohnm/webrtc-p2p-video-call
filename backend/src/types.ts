851b469d539dbabf62b38af2f1b93459f99f36ae
martinjohnm <johnmmartin74@gmail.com>
2025-01-03 03:22:47 +0530

    types changed

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -3,5 +3,5 @@
 
 export const IDENTIFY_AS_SENDER = "identify-as-sender"
 export const IDENTIFY_AS_RECEIVER = "identify-as-receiver"
-export const CREATE_OFFER = "create-offer"
-export const CREATE_ANSWER = "create-answer"
+export const CREATE_OFFER = "createOffer"
+export const CREATE_ANSWER = "createAnswer"
